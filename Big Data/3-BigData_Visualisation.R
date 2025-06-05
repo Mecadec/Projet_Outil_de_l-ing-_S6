f102a4530cb5df8b895e9935659449433d5095b6
Mecadec <gauthiergloanec@yahoo.fr>
2025-06-05 16:51:51 +0200

    Update 3-BigData_Visualisation.R
    ça marche pas mais je bosse dessus

--- Big Data/3-BigData_Visualisation.R
+++ Big Data/3-BigData_Visualisation.R
@@ -1,10 +1,12 @@
-library(shiny)         # Interface web
-library(tidyverse)     # Manipulation de données
-library(leaflet)       # Carte interactive
-library(lubridate)     # Gestion des dates
-library(leaflet.extras) # Extensions Leaflet (heatmap)
+library(shiny)
+library(tidyverse)
+library(leaflet)
+library(lubridate)
+library(leaflet.extras)
+library(geosphere)
+library(dbscan)
 
-# Chargement des données
+# Chargement et préparation des données
 df <- read_csv("C:/Users/Gauth/OneDrive/Documents/GitHub/Projet_Outil_de_l-ing-_S6/Big Data/Data/After_Sort.csv") %>%
   mutate(
     BaseDateTime = suppressWarnings(ymd_hms(BaseDateTime)),
@@ -19,26 +21,26 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("bateau", "Choisir un bateau :", choices = unique(df$VesselName)),
       checkboxInput("show_heatmap", "Afficher les routes fréquentées", value = FALSE),
-      tableOutput("infos_bateau")  # Affichage des infos
+      checkboxInput("show_ports", "Afficher les ports détectés", value = TRUE),
+      tableOutput("infos_bateau")
     ),
     mainPanel(
-      leafletOutput("map", height = "700px", width = "100%")
+      leafletOutput("map", height = "600px", width = "100%")
     )
   )
 )
 
 # Serveur
 server <- function(input, output, session) {
-  
-  # Carte initiale vide
+
   output$map <- renderLeaflet({
-    leaflet() %>% addTiles()
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = mean(df$LON, na.rm = TRUE), lat = mean(df$LAT, na.rm = TRUE), zoom = 6)
   })
-  
-  # Infos du bateau sélectionné (clé/valeur, types homogènes)
+
   output$infos_bateau <- renderTable({
     req(input$bateau)
-    
     df %>%
       filter(VesselName == input$bateau) %>%
       select(MMSI, IMO, CallSign, VesselType, Length, Width, Draft, Cargo, TransceiverClass) %>%
@@ -46,16 +48,15 @@ server <- function(input, output, session) {
       mutate(across(everything(), as.character)) %>%
       pivot_longer(everything(), names_to = "Champ", values_to = "Valeur")
   }, striped = TRUE, bordered = TRUE, colnames = FALSE)
-  
-  # Affichage trajectoire
+
   observeEvent(input$bateau, {
     req(input$bateau)
-    
+
     df_bateau <- df %>%
       filter(VesselName == input$bateau) %>%
       arrange(BaseDateTime) %>%
       mutate(time_diff = as.numeric(difftime(BaseDateTime, lag(BaseDateTime), units = "hours")))
-    
+
     interpolate_points <- function(p1, p2, interval_mins = 15) {
       t_seq <- seq(p1$BaseDateTime, p2$BaseDateTime, by = paste(interval_mins, "mins"))
       t_seq <- t_seq[-c(1, length(t_seq))]
@@ -66,7 +67,7 @@ server <- function(input, output, session) {
         LON = approx(c(p1$BaseDateTime, p2$BaseDateTime), c(p1$LON, p2$LON), xout = t_seq)$y
       )
     }
-    
+
     traj_full <- list()
     for (i in 2:nrow(df_bateau)) {
       prev <- df_bateau[i - 1, ]
@@ -80,10 +81,10 @@ server <- function(input, output, session) {
         }
       }
     }
-    
+
     traj_full <- bind_rows(traj_full, df_bateau[nrow(df_bateau), ]) %>%
       arrange(BaseDateTime)
-    
+
     leafletProxy("map") %>%
       clearShapes() %>%
       clearMarkers() %>%
@@ -103,33 +104,103 @@ server <- function(input, output, session) {
         opacity = 0.7
       )
   })
-  
-  # Heatmap
+
   observe({
     proxy <- leafletProxy("map")
-    proxy %>% clearGroup("heatmap") %>% clearControls()
+    proxy %>% clearGroup("ports") %>% clearGroup("flux_ports")
     
-    if (input$show_heatmap) {
+    if (!input$show_ports) return()
+    
+    ports_points <- df %>%
+      filter(Status == 5) %>%
+      select(BaseDateTime, LAT, LON, MMSI, Cargo) %>%
+      drop_na(LAT, LON)
+    
+    if (nrow(ports_points) == 0) return()
+    
+    clusters <- dbscan(ports_points %>% select(LAT, LON), eps = 0.045, minPts = 1)
+    ports_points$cluster <- clusters$cluster
+    
+    ports_summary <- ports_points %>%
+      group_by(cluster) %>%
+      summarise(
+        lat = mean(LAT),
+        lon = mean(LON),
+        nb_bateaux = n_distinct(MMSI),
+        cargaison_freq = names(sort(table(Cargo), decreasing = TRUE))[1],
+        .groups = "drop"
+      ) %>%
+      mutate(port_name = paste("Port", cluster))
+    
+    proxy %>%
+      addMarkers(
+        data = ports_summary,
+        lng = ~lon,
+        lat = ~lat,
+        group = "ports",
+        clusterOptions = markerClusterOptions(),
+        label = ~paste0(
+          port_name, "<br>",
+          "Bateaux passés : ", nb_bateaux, "<br>",
+          "Cargaison fréquente : ", cargaison_freq
+        ) %>% lapply(htmltools::HTML)
+      )
+    
+    # Calcul du flux entre ports
+    traj_ports <- ports_points %>%
+      arrange(MMSI, BaseDateTime) %>%
+      group_by(MMSI) %>%
+      mutate(next_cluster = lead(cluster)) %>%
+      filter(!is.na(next_cluster) & cluster != next_cluster) %>%
+      ungroup() %>%
+      count(cluster, next_cluster, sort = TRUE)
+    
+    # Garder seulement le flux sortant principal par port
+    traj_ports <- traj_ports %>%
+      group_by(cluster) %>%
+      slice_max(n, n = 1, with_ties = FALSE) %>%
+      ungroup() %>%
+      left_join(ports_summary %>% select(cluster, lat1 = lat, lon1 = lon), by = "cluster") %>%
+      left_join(ports_summary %>% select(next_cluster = cluster, lat2 = lat, lon2 = lon), by = "next_cluster")
+    
+    # Tracer lignes + flèches directionnelles
+    for (i in 1:nrow(traj_ports)) {
+      p1 <- c(traj_ports$lon1[i], traj_ports$lat1[i])
+      p2 <- c(traj_ports$lon2[i], traj_ports$lat2[i])
+      w <- 2
+      
+      # ligne principale
       proxy %>%
-        addHeatmap(
-          data = df,
-          lng = ~LON,
-          lat = ~LAT,
-          radius = 8,
-          blur = 15,
-          max = 0.05,
-          group = "heatmap"
-        ) %>%
-        addLegend(
-          position = "bottomright",
-          colors = c("blue", "limegreen", "yellow", "orange", "red"),
-          labels = c("Peu fréquenté", "", "", "", "Très fréquenté"),
-          title = "Trafic maritime",
-          opacity = 0.7
+        addPolylines(
+          lng = c(p1[1], p2[1]),
+          lat = c(p1[2], p2[2]),
+          color = "blue",
+          weight = w,
+          opacity = 0.5,
+          group = "flux_ports"
+        )
+      
+      # flèche triangulaire
+      bearing_angle <- bearing(p1, p2)
+      arrow_len <- 10 # km
+      arrow_base <- destPoint(p2, bearing_angle + 180, arrow_len)
+      left <- destPoint(arrow_base, bearing_angle + 135, arrow_len / 2)
+      right <- destPoint(arrow_base, bearing_angle - 135, arrow_len / 2)
+      
+      proxy %>%
+        addPolygons(
+          lng = c(left[1], p2[1], right[1]),
+          lat = c(left[2], p2[2], right[2]),
+          color = "blue",
+          fillColor = "blue",
+          fillOpacity = 0.9,
+          weight = 1,
+          group = "flux_ports"
         )
     }
   })
+  
+
 }
 
-# Lancer l'app
 shinyApp(ui, server)
